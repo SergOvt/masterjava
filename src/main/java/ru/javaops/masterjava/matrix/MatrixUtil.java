740a7e05f4beddce552371d09387c6e699783b44
Ovchinnikov Sergey <s.ovchinnikov@bpcbt.com>
2019-04-30 18:45:24 +0300

    HW1

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -4,10 +4,6 @@ import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 
-/**
- * gkislin
- * 03.07.2016
- */
 public class MatrixUtil {
 
     // TODO implement parallel multiplication matrixA*matrixB
@@ -15,6 +11,21 @@ public class MatrixUtil {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
+        int[] thatColumn = new int[matrixSize];
+        for (int j = 0; j < matrixSize; j++) {
+            for (int k = 0; k < matrixSize; k++) {
+                thatColumn[k] = matrixB[k][j];
+            }
+
+            for (int i = 0; i < matrixSize; i++) {
+                int[] thisRow = matrixA[i];
+                int summand = 0;
+                for (int k = 0; k < matrixSize; k++) {
+                    summand += thatColumn[k] * thisRow[k];
+                }
+                matrixC[i][j] = summand;
+            }
+        }
         return matrixC;
     }
 
@@ -23,13 +34,19 @@ public class MatrixUtil {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                int sum = 0;
+        int[] thatColumn = new int[matrixSize];
+        for (int j = 0; j < matrixSize; j++) {
+            for (int k = 0; k < matrixSize; k++) {
+                thatColumn[k] = matrixB[k][j];
+            }
+
+            for (int i = 0; i < matrixSize; i++) {
+                int[] thisRow = matrixA[i];
+                int summand = 0;
                 for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
+                    summand += thatColumn[k] * thisRow[k];
                 }
-                matrixC[i][j] = sum;
+                matrixC[i][j] = summand;
             }
         }
         return matrixC;
